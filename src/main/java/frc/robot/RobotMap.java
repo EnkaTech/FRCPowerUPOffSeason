554bbae12657106fb6c9830d4b3190ebbb3eb13a
Baran <b.marap@gmail.com>
2018-09-16 15:10:18 +0300

    Initial Commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+  public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+  public static int driveTrainFL = 1;
+  public static int driveTrainFR = 2;
+  public static int driveTrainRL = 3;
+  public static int driveTrainRR = 4;
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  // public static int leftMotor = 1;
+  // public static int rightMotor = 2;
+
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+}
